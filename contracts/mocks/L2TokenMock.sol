201fe965b0e209fcdf0065ef57f44d0d1d771896
Alex Skipper <skipper@onbridge.io>
2021-10-30 16:06:17 +0300

    feat: L2Token with tests

---
+++ contracts/mocks/L2TokenMock.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPLv3
+
+pragma solidity ^0.8.9;
+
+import "../L2Token.sol";
+
+/**
+ * @title ERC721Mock
+ * This mock just provides a public safeMint, mint, and burn functions for testing purposes
+ */
+contract L2TokenMock is L2Token {
+
+    function safeMint(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId);
+    }
+
+    function safeMint(
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public {
+        _safeMint(to, tokenId, _data);
+    }
+
+    function burn(uint256 tokenId) public {
+        _burn(tokenId);
+    }
+}
