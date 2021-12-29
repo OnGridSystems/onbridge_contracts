e33aa5f42404ef78e91348144f6adf52efc37b56
Kirill Varlamov <kirill@ongrid.pro>
2021-12-29 18:16:21 +0300

    fix: wrong l2bridge address length using abi.encodePacked

--- contracts/L1Bridge.sol
+++ contracts/L1Bridge.sol
@@ -86,7 +86,7 @@ contract L1Bridge is AccessControl {
         SubmissionAutoParamsTo memory autoParams;
         autoParams.flags = 2 ** REVERT_IF_EXTERNAL_FAIL + 2 ** PROXY_WITH_SENDER;
         autoParams.executionFee = 30000000000000000;
-        autoParams.fallbackAddress = abi.encode(l2bridge);
+        autoParams.fallbackAddress = abi.encodePacked(l2bridge);
         autoParams.data = abi.encodeWithSignature("finalizeInboundTransfer(address,string,uint256)", l2bridge, "", _id);
         l1DeBridgeGate.send{value: msg.value}(address(0), msg.value, chainIdTo, abi.encodePacked(l2bridge), permit, false, referralCode, abi.encode(autoParams));
         emit DepositInitiated(address(l1Token), msg.sender, _to, _id);
