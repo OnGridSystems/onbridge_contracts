87819bcec09c54656563dfe7516f2a99057cdc09
OnBridge Captain <captain@onbridge.io>
2021-12-29 17:09:45 +0300

    feat(debridge): redeploy l1 side on Kovan testnet

--- deploy/l1_bridge.js
+++ deploy/l1_bridge.js
@@ -2,17 +2,19 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deployer } = await getNamedAccounts();
   const { deploy } = deployments;
 
-  // const L1Token = await ethers.getContract("L1Token")
-  const L1Token = "0x97ea372254e8bF2Da32fD2756934d798C12bef54";
+  const L1Token = await ethers.getContract("L1Token");
+  const DeBridgeGate = await ethers.getContract("DeBridgeGateMock");
+
   // const L2Token = await ethers.getContract("L2Token")
-  const L2Token = "0x49d25DD6a5BC2993c1A4762F826065940909Ee5F";
+  const L2Token = "0x31649ac584eBcBfD465b49304eB27574B2111F0B";
+  const L2Bridge = "0x463b31B32417f122d25cbAF84Ff263723c11de64";
 
   console.log("deploying L1Bridge");
 
   await deploy("L1Bridge", {
     from: deployer,
     log: true,
-    args: [L1Token, L2Token],
+    args: [L1Token.address, DeBridgeGate.address, L2Token, L2Bridge],
     skipIfAlreadyDeployed: true,
   });
 };
