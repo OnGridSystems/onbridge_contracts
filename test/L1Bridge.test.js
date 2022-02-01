78fe8527862eabd70e276a15ecd8047ddc6c59be
Alex Skipper <skipper@onbridge.io>
2022-02-02 01:45:24 +0300

    wip: update tests for new oracles

--- test/L1Bridge.test.js
+++ test/L1Bridge.test.js
@@ -4,7 +4,7 @@ const { constants } = require("@openzeppelin/test-helpers");
 describe("L1Bridge", function () {
   before(async function () {
     const accounts = await ethers.getSigners();
-    this.owner = accounts[0];
+    this.deployer = accounts[0];
     this.account1 = accounts[1];
     // code on L1 is not callable directly, so we take just random address
     this.l2Token = accounts[2];
@@ -25,7 +25,16 @@ describe("L1Bridge", function () {
       this.L1BridgeArtifact.deploy(
         constants.ZERO_ADDRESS,
         this.account1.address,
+        this.account1.address
+      )
+    ).to.be.revertedWith("ZERO_TOKEN");
+  });
+
+  it("revert then trying finalizeInboundTransfer to zero address", async function () {
+    await expect(
+      this.L1BridgeArtifact.deploy(
         this.account1.address,
+        constants.ZERO_ADDRESS,
         this.account1.address
       )
     ).to.be.revertedWith("ZERO_TOKEN");
@@ -36,23 +45,30 @@ describe("L1Bridge", function () {
       this.L1BridgeArtifact.deploy(
         this.account1.address,
         this.account1.address,
-        constants.ZERO_ADDRESS,
-        this.account1.address
+        constants.ZERO_ADDRESS
       )
-    ).to.be.revertedWith("ZERO_TOKEN");
+    ).to.be.revertedWith("ZERO_DEBRIDGEGATE");
   });
 
   beforeEach(async function () {
+    this.l2ChainId = 97;
+
     this.l1token = await this.L1TokenArtifact.deploy();
     this.l1debridgeGate = await this.DeBridgeGateArtifact.deploy();
     this.l1bridge = await this.L1BridgeArtifact.deploy(
       this.l1token.address,
-      this.l1debridgeGate.address,
       this.l2Token.address,
-      this.l2bridge.address
+      this.l1debridgeGate.address
     );
-    const ORACLE_ROLE = await this.l1bridge.ORACLE_ROLE();
-    await this.l1bridge.grantRole(ORACLE_ROLE, this.oracle.address);
+
+    await this.l1token.grantRole(
+      await this.l1token.DEFAULT_ADMIN_ROLE(),
+      this.deployer.address
+    );
+    await this.l1bridge.grantRole(
+      await this.l1bridge.DEFAULT_ADMIN_ROLE(),
+      this.deployer.address
+    );
     await this.l1token.grantRole(
       await this.l1token.MINTER_ROLE(),
       this.minterAdmin.address
@@ -63,14 +79,14 @@ describe("L1Bridge", function () {
     );
   });
 
-  it("l1 token has proper name and symbol", async function () {
-    expect(await this.l1token.name()).to.equal("Grizzly");
-    expect(await this.l1token.symbol()).to.equal("GRZL");
-  });
-
   it("should be deployed", async function () {
     expect(await this.l1bridge.deployed(), true);
     expect(await this.l1token.deployed(), true);
+  });
+
+  it("l1 token has proper name and symbol", async function () {
+    expect(await this.l1token.name()).to.equal("Grizzly");
+    expect(await this.l1token.symbol()).to.equal("GRZL");
   });
 
   it("bridge has zero balance", async function () {
@@ -88,64 +104,81 @@ describe("L1Bridge", function () {
       expect(await this.l1token.balanceOf(this.l1holder.address)).to.equal("1");
     });
 
-    describe("l1holder transfer 100 token to l1bridge", function () {
+    describe("add controlling address to l1bridge", function () {
       beforeEach(async function () {
-        await this.l1token
-          .connect(this.l1holder)
-          .approve(this.l1bridge.address, 100);
-        this.outboundTransferTx = await this.l1bridge
-          .connect(this.l1holder)
-          .outboundTransfer(this.l1holder.address, 100, { value: 123456 });
+        await this.l1bridge
+          .connect(this.deployer)
+          .setContractAddressOnChainId(this.l2bridge.address, this.l2ChainId);
+
+        await this.l1bridge
+          .connect(this.deployer)
+          .addControllingAddress(this.l2bridge.address, this.l2ChainId);
       });
 
-      it("check events of outboundTransferTx transacted deBridgeGate", async function () {
-        const PROXY_WITH_SENDER_POSITION = 2;
-        const REVERT_IF_EXTERNAL_FAIL_POSITION = 1;
-        const executionFee = "30000000000000000";
-        const flags =
-          2 ** REVERT_IF_EXTERNAL_FAIL_POSITION +
-          2 ** PROXY_WITH_SENDER_POSITION;
-        const fallbackAddress = this.l2bridge.address.toLowerCase();
-        //todo: use l2 bridge interface to be consistent
-        const data = this.l1bridge.interface.encodeFunctionData(
-          "finalizeInboundTransfer",
-          [this.l1holder.address, "", 100]
-        );
-        await expect(this.outboundTransferTx).to.emit(
-          this.l1debridgeGate,
-          "Sent"
-        );
-        await expect(this.outboundTransferTx)
-          .to.emit(this.l1debridgeGate, "Log")
-          .withArgs([executionFee, flags, fallbackAddress, data]);
-      });
-
-      it("l1bridge is ownerOf 100 token", async function () {
-        expect(await this.l1token.ownerOf(100)).to.equal(this.l1bridge.address);
-        expect(await this.l1token.balanceOf(this.l1bridge.address)).to.equal(
-          "1"
-        );
-      });
-
-      describe("l1holder transfer 100 token to l1bridge", function () {
+      describe("approving 100 token to l1 bridge and outboundTransfer to l2 bridge", function () {
         beforeEach(async function () {
-          const l2Tx =
-            "0x117ddadadc7b8d342cf48513fef06a2cba15dfb9c488dc51aefc998abcefb52b";
-          await this.l1bridge
-            .connect(this.oracle)
-            .finalizeInboundTransfer(this.l1holder.address, l2Tx, 100);
+          await this.l1token
+            .connect(this.l1holder)
+            .approve(this.l1bridge.address, 100);
         });
 
-        it("l1holder is 100 token owner", async function () {
-          expect(await this.l1token.ownerOf(100)).to.equal(
-            this.l1holder.address
-          );
-          expect(await this.l1token.balanceOf(this.l1bridge.address)).to.equal(
-            "0"
-          );
-          expect(await this.l1token.balanceOf(this.l1holder.address)).to.equal(
-            "1"
-          );
+        it("100 token can be outbounded to l2 bridge", async function () {
+          await this.l1bridge
+            .connect(this.l1holder)
+            .outboundTransfer(
+              this.l1holder.address,
+              100,
+              this.l2ChainId,
+              this.l2bridge.address,
+              ethers.utils.parseEther("0.01"),
+              { value: ethers.utils.parseEther("0.03") }
+            );
+        });
+
+        describe("outboundTransfer 100 token to l2 bridge", function () {
+          beforeEach(async function () {
+            this.outboundTransferTx = await this.l1bridge
+              .connect(this.l1holder)
+              .outboundTransfer(
+                this.l1holder.address,
+                100,
+                this.l2ChainId,
+                this.l2bridge.address,
+                ethers.utils.parseEther("0.01"),
+                { value: ethers.utils.parseEther("0.03") }
+              );
+          });
+
+          it("check events of outboundTransferTx transacted deBridgeGate", async function () {
+            const PROXY_WITH_SENDER_POSITION = 2;
+            const REVERT_IF_EXTERNAL_FAIL_POSITION = 1;
+            const executionFee = ethers.utils.parseEther("0.01");
+            const flags =
+              2 ** REVERT_IF_EXTERNAL_FAIL_POSITION +
+              2 ** PROXY_WITH_SENDER_POSITION;
+            const fallbackAddress = this.l2bridge.address.toLowerCase();
+            //todo: use l2 bridge interface to be consistent
+            const data = this.l1bridge.interface.encodeFunctionData(
+              "finalizeInboundTransfer",
+              [this.l1holder.address, "", 100]
+            );
+            await expect(this.outboundTransferTx).to.emit(
+              this.l1debridgeGate,
+              "Sent"
+            );
+            await expect(this.outboundTransferTx)
+              .to.emit(this.l1debridgeGate, "Log")
+              .withArgs([executionFee, flags, fallbackAddress, data]);
+          });
+
+          it("l1bridge is ownerOf 100 token", async function () {
+            expect(await this.l1token.ownerOf(100)).to.equal(
+              this.l1bridge.address
+            );
+            expect(
+              await this.l1token.balanceOf(this.l1bridge.address)
+            ).to.equal("1");
+          });
         });
       });
     });
--- test/L2Bridge.test.js
+++ test/L2Bridge.test.js
@@ -4,41 +4,79 @@ const { constants } = require("@openzeppelin/test-helpers");
 describe("L2Bridge", function () {
   before(async function () {
     const accounts = await ethers.getSigners();
-    this.owner = accounts[0];
+    this.deployer = accounts[0];
     this.account1 = accounts[1];
     // code on L1 is not callable directly, so we take just random address
     this.l1Token = accounts[2];
     this.oracle = accounts[3];
     this.l2holder = accounts[4];
     this.minterAdmin = accounts[5];
+    this.l1bridge = accounts[6];
 
     this.L2TokenArtifact = await ethers.getContractFactory("L2Token");
     this.L2BridgeArtifact = await ethers.getContractFactory("L2Bridge");
+    this.DeBridgeGateArtifact = await ethers.getContractFactory(
+      "DeBridgeGateMock"
+    );
   });
 
   it("revert then trying finalizeInboundTransfer to zero address", async function () {
     await expect(
-      this.L2BridgeArtifact.deploy(constants.ZERO_ADDRESS, this.l2token.address)
+      this.L2BridgeArtifact.deploy(
+        constants.ZERO_ADDRESS,
+        this.account1.address,
+        this.account1.address
+      )
     ).to.be.revertedWith("ZERO_TOKEN");
   });
 
   it("revert then trying finalizeInboundTransfer to zero address", async function () {
     await expect(
-      this.L2BridgeArtifact.deploy(this.l1Token.address, constants.ZERO_ADDRESS)
+      this.L2BridgeArtifact.deploy(
+        this.account1.address,
+        constants.ZERO_ADDRESS,
+        this.account1.address
+      )
     ).to.be.revertedWith("ZERO_TOKEN");
+  });
+
+  it("revert then trying finalizeInboundTransfer to zero address", async function () {
+    await expect(
+      this.L2BridgeArtifact.deploy(
+        this.account1.address,
+        this.account1.address,
+        constants.ZERO_ADDRESS
+      )
+    ).to.be.revertedWith("ZERO_DEBRIDGEGATE");
   });
 
   beforeEach(async function () {
+    this.l1ChainId = 42;
+
     this.l2token = await this.L2TokenArtifact.deploy();
+    this.l2debridgeGate = await this.DeBridgeGateArtifact.deploy();
     this.l2bridge = await this.L2BridgeArtifact.deploy(
+      this.l2token.address,
       this.l1Token.address,
-      this.l2token.address
+      this.l2debridgeGate.address
     );
-    const MINTER_ROLE = await this.l2token.MINTER_ROLE();
-    await this.l2token.grantRole(MINTER_ROLE, this.minterAdmin.address);
-    await this.l2token.grantRole(MINTER_ROLE, this.l2bridge.address);
-    const ORACLE_ROLE = await this.l2bridge.ORACLE_ROLE();
-    await this.l2bridge.grantRole(ORACLE_ROLE, this.oracle.address);
+
+    await this.l2token.grantRole(
+      await this.l2token.DEFAULT_ADMIN_ROLE(),
+      this.deployer.address
+    );
+    await this.l2bridge.grantRole(
+      await this.l2bridge.DEFAULT_ADMIN_ROLE(),
+      this.deployer.address
+    );
+    await this.l2token.grantRole(
+      await this.l2token.MINTER_ROLE(),
+      this.minterAdmin.address
+    );
+    await this.l2token.grantRole(
+      await this.l2token.DEFAULT_ADMIN_ROLE(),
+      this.minterAdmin.address
+    );
   });
 
   it("should be deployed", async function () {
@@ -51,78 +89,86 @@ describe("L2Bridge", function () {
     expect(await this.l2token.symbol()).to.equal("GRZL");
   });
 
-  it("revert then trying finalizeInboundTransfer to zero address", async function () {
-    const l1Tx =
-      "0x117ddadadc7b8d342cf48513fef06a2cba15dfb9c488dc51aefc998abcefb52b";
-    await expect(
-      this.l2bridge
-        .connect(this.oracle)
-        .finalizeInboundTransfer(constants.ZERO_ADDRESS, l1Tx, "1")
-    ).to.be.revertedWith("Token cannot be the zero address");
-  });
-
-  describe("oracle calls finalizeInboundTransfer", function () {
-    const l1Tx =
-      "0x117ddadadc7b8d342cf48513fef06a2cba15dfb9c488dc51aefc998abcefb52b";
+  describe("add controlling address to l2bridge", function () {
     beforeEach(async function () {
+      // TODO replase token income test branch
+      await this.l2token
+        .connect(this.minterAdmin)
+        .mint(this.l2holder.address, 100);
+
       await this.l2bridge
-        .connect(this.oracle)
-        .finalizeInboundTransfer(this.l2holder.address, l1Tx, "100");
+        .connect(this.deployer)
+        .setContractAddressOnChainId(this.l1bridge.address, this.l1ChainId);
+
+      await this.l2bridge
+        .connect(this.deployer)
+        .addControllingAddress(this.l1bridge.address, this.l1ChainId);
     });
 
-    it("l2token supply increased", async function () {
-      expect(await this.l2token.totalSupply()).to.equal("1");
-    });
-
-    it("l2token balance of holder increased", async function () {
-      expect(await this.l2token.balanceOf(this.l2holder.address)).to.equal("1");
-    });
-
-    it("l2holder is 100 token owner", async function () {
-      await expect(
-        this.l2token.connect(this.minterAdmin).mint(this.l2holder.address, 100)
-      ).to.be.revertedWith("ERC721: token already minted");
-      expect(await this.l2token.ownerOf(100)).to.equal(this.l2holder.address);
-    });
-
-    describe("oracle calls finalizeInboundTransfer", function () {
-      const l1Tx =
-        "0x117ddadadc7b8d342cf48513fef06a2cba15dfb9c488dc51aefc998abcefb52b";
-      beforeEach(async function () {
-        await this.l2bridge
-          .connect(this.oracle)
-          .finalizeInboundTransfer(this.l2holder.address, l1Tx, "200");
-      });
-
-      it("l2bridge can`t burn a token that the l2holder does not own", async function () {
-        await expect(
-          this.l2bridge
-            .connect(this.l2holder)
-            .outboundTransfer(this.l2holder.address, "200")
-        ).to.be.revertedWith(
-          "ERC721: transfer caller is not owner nor approved"
-        );
-      });
-    });
-
-    describe("token goes back to L1 (holder calls outboundTransfer)", function () {
+    describe("approving 100 token to l2 bridge and outboundTransfer to l2 bridge", function () {
       beforeEach(async function () {
         await this.l2token
           .connect(this.l2holder)
-          .approve(this.l2bridge.address, "100");
+          .approve(this.l2bridge.address, 100);
+      });
+
+      it("100 token can be outbounded to l2 bridge", async function () {
         await this.l2bridge
           .connect(this.l2holder)
-          .outboundTransfer(this.l2holder.address, "100");
+          .outboundTransfer(
+            this.l2holder.address,
+            100,
+            this.l1ChainId,
+            this.l1bridge.address,
+            ethers.utils.parseEther("0.01"),
+            { value: ethers.utils.parseEther("0.03") }
+          );
       });
 
-      it("l2token supply decreased", async function () {
-        expect(await this.l2token.totalSupply()).to.equal("0");
-      });
+      describe("outboundTransfer 100 token to l2 bridge", function () {
+        beforeEach(async function () {
+          this.outboundTransferTx = await this.l2bridge
+            .connect(this.l2holder)
+            .outboundTransfer(
+              this.l2holder.address,
+              100,
+              this.l1ChainId,
+              this.l2bridge.address,
+              ethers.utils.parseEther("0.01"),
+              { value: ethers.utils.parseEther("0.03") }
+            );
+        });
 
-      it("l2token balance of holder decreased", async function () {
-        expect(await this.l2token.balanceOf(this.l2holder.address)).to.equal(
-          "0"
-        );
+        it("check events of outboundTransferTx transacted deBridgeGate", async function () {
+          const PROXY_WITH_SENDER_POSITION = 2;
+          const REVERT_IF_EXTERNAL_FAIL_POSITION = 1;
+          const executionFee = ethers.utils.parseEther("0.01");
+          const flags =
+            2 ** REVERT_IF_EXTERNAL_FAIL_POSITION +
+            2 ** PROXY_WITH_SENDER_POSITION;
+          const fallbackAddress = this.l2bridge.address.toLowerCase();
+          //todo: use l2 bridge interface to be consistent
+          const data = this.l2bridge.interface.encodeFunctionData(
+            "finalizeInboundTransfer",
+            [this.l2holder.address, "", 100]
+          );
+          await expect(this.outboundTransferTx).to.emit(
+            this.l2debridgeGate,
+            "Sent"
+          );
+          await expect(this.outboundTransferTx)
+            .to.emit(this.l2debridgeGate, "Log")
+            .withArgs([executionFee, flags, fallbackAddress, data]);
+        });
+
+        it("l2bridge is ownerOf 100 token", async function () {
+          expect(await this.l2token.ownerOf(100)).to.equal(
+            this.l2bridge.address
+          );
+          expect(await this.l2token.balanceOf(this.l2bridge.address)).to.equal(
+            "1"
+          );
+        });
       });
     });
   });
