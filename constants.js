62788724579a75d5d188c5370977f376b46ad86f
OnBridge Cook <cook@onbridge.io>
2022-04-07 18:27:22 +0300

    deploy: redeploy bridges and tokens fo L1/L2 #ONBD-101

--- constants.js
+++ constants.js
@@ -1,18 +1,18 @@
 const DeBridgeGate = "0x68D936Cb4723BdD38C488FD50514803f96789d2D";
 
-const tokenId = 0;
+const tokenId = 1;
 
 const bscChainId = 97;
 const ethChainId = 42;
 const polygonChainId = 80001;
 
-const bscToken = "0xbceEEA68fC0E50e0DE2e5473dE23CED1a7397629";
-const ethToken = "0xf2391B61D7CC9Bb31D8BeC8Df7d05B3B5Dd97939";
-const polygonToken = "0xeddB5E4b1199Cc409eaFB98EcF1D7f2b18fA51Af";
+const bscToken = "0x0dB2EB9b77dc0fDF130d29D338763C0eDc7CA02D";
+const ethToken = "0x7EAD12149175470D91d1BEA465c1D6ee3C5E7E1A";
+const polygonToken = "0x81a6e27FE7659B210e9Fd7C30765Eb268294C9aA";
 
-const bscBridge = "0xcA2DaaDAD22BD98c020481A360344B31cD63251e";
-const ethBridge = "0x5EE16fBe8f8dBB2bb39f3ac2319bE891c2685EA4";
-const polygonBridge = "0x89903eDda80616c9fC0bc01dE408eB8AfC75f09d";
+const bscBridge = "0xc2d7c0C63FFB42924e1A2f4Ce487A10670248Ac6";
+const ethBridge = "0x98f8a9Ce7aA9dF2fe6C3931Dad6bf654e59d7C01";
+const polygonBridge = "0x7123d1e4fDC36300C083fE7276e8429EBC3F6d07";
 
 module.exports = {
   DeBridgeGate,
